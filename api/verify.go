65ef60f1d84eaf639baef41f075968323fa3456e
sblii <lidusdls@gmail.com>
2026-08-25 23:07:38 +0900

    Update verify.go

--- api/verify.go
+++ api/verify.go
@@ -4,6 +4,7 @@ import (
 	"encoding/json"
 	"net/http"
 	"os"
+	"time"
 )
 
 type RequestBody struct {
@@ -33,14 +34,17 @@ func Handler(w http.ResponseWriter, r *http.Request) {
 
 	//Token
 	sessionToken := os.Getenv("TOKEN") // Vercel 환경변수에 등록해야 함
+	//Expiration Time declare (MACOS)
+	expiration := time.Now().Add(1*time.Hour)
 	http.SetCookie(w, &http.Cookie{
 		Name:     "auth_session",
 		Value:    sessionToken,
 		Path:     "/",
 		MaxAge:   3600, 
+		Expires: expiration,
 		HttpOnly: true, 
 		Secure:   true, 
-		SameSite: http.SameSiteStrictMode,
+		SameSite: http.SameSiteLaxMode,
 	})
 
 	w.Header().Set("Content-Type", "application/json; charset=utf-8")
