3314d6ede1dbaec3c035a6ebd0918f80d0544d74
sblii <lidusdls@gmail.com>
2026-08-25 22:55:07 +0900

    Update verify.go

--- api/verify.go
+++ api/verify.go
@@ -11,11 +11,13 @@ type RequestBody struct {
 }
 
 func Handler(w http.ResponseWriter, r *http.Request) {
+	//Exception Handler 1 : HTTP METHOD
 	if r.Method != http.MethodPost {
 		http.Error(w, `{"success": false, "error": "Method not allowed"}`, http.StatusMethodNotAllowed)
 		return
 	}
 
+	//Exception Handler 2 : JSON FORMAT
 	var req RequestBody
 	err := json.NewDecoder(r.Body).Decode(&req)
 	if err != nil {
@@ -23,14 +25,25 @@ func Handler(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	envPassword := os.Getenv("PASSWORD")
-
-	if req.Password != envPassword {
-		http.Error(w, `{"success": false, "error": "Unauthorized: 비밀번호가 틀렸습니다."}`, http.StatusUnauthorized)
+	//Exception Handler 3 : Wrong password
+	if req.Password != os.Getenv("PASSWORD") {
+		http.Error(w, `{"success": false, "error": "Unauthorized: Wrong Password."}`, http.StatusUnauthorized)
 		return
 	}
 
+	//Token
+	sessionToken := os.Getenv("TOKEN") // Vercel 환경변수에 등록해야 함
+	http.SetCookie(w, &http.Cookie{
+		Name:     "auth_session",
+		Value:    sessionToken,
+		Path:     "/",
+		MaxAge:   3600, 
+		HttpOnly: true, 
+		Secure:   true, 
+		SameSite: http.SameSiteStrictMode,
+	})
+
 	w.Header().Set("Content-Type", "application/json; charset=utf-8")
 	w.WriteHeader(http.StatusOK)
-	w.Write([]byte(`{"success": true, "message": "인증 완료되었습니다."}`))
+	w.Write([]byte(`{"success": true, "message": "Authorized"}`))
 }
